e3f8a6495204b55ce544241cf3333897c4680967
Jorge Morales <jorge.iv.morales@gmail.com>
2019-08-13 23:37:51 -0500

    Agregando info de speakers

--- src/app/home/speakers/speakers.component.ts
+++ src/app/home/speakers/speakers.component.ts
@@ -12,70 +12,50 @@ export class SpeakersComponent implements OnInit {
     ngOnInit() {
         this.speakers = [
             {
-                name: 'Gabriel Trujillo',
+                name: 'Adrian Estrada',
+                title: 'Tech Manager',
+                picture:
+                    'https://s.gravatar.com/avatar/b6e74cc5bf30b12fe6346c6b4ba3db25?s=400&r=g',
+                bio:
+                    'Developer in love with Node.js, thirsty for new technologies, working at @NodeSource, official Node.js core Collaborator,  organizer at @MedellinJS',
+            },
+            {
+                name: 'Diego Valderrama',
                 title: 'Tech Manager',
                 picture: '../../../../assets/img/man.png',
                 bio:
                     "Here is where i'd put the bio ... if i had one! (ノಠ益ಠ)ノ彡┻━┻",
             },
             {
-                name: 'Gabriel Trujillo',
+                name: 'Oscar Granada',
+                title: 'Tech Manager',
+                picture: '../../../../assets/img/oscarGranada.png',
+                bio:
+                    "Here is where i'd put the bio ... if i had one! (ノಠ益ಠ)ノ彡┻━┻",
+            },
+            {
+                name: 'Alejandro Peña',
                 title: 'Tech Manager',
                 picture: '../../../../assets/img/man.png',
                 bio:
                     "Here is where i'd put the bio ... if i had one! (ノಠ益ಠ)ノ彡┻━┻",
             },
             {
-                name: 'Gabriel Trujillo',
+                name: 'Olga Saucedo',
                 title: 'Tech Manager',
                 picture: '../../../../assets/img/man.png',
                 bio:
                     "Here is where i'd put the bio ... if i had one! (ノಠ益ಠ)ノ彡┻━┻",
             },
             {
-                name: 'Gabriel Trujillo',
+                name: 'Ana Sosa',
                 title: 'Tech Manager',
-                picture: '../../../../assets/img/man.png',
+                picture: '../../../../assets/img/anaSosa.png',
                 bio:
                     "Here is where i'd put the bio ... if i had one! (ノಠ益ಠ)ノ彡┻━┻",
             },
             {
-                name: 'Gabriel Trujillo',
-                title: 'Tech Manager',
-                picture: '../../../../assets/img/man.png',
-                bio:
-                    "Here is where i'd put the bio ... if i had one! (ノಠ益ಠ)ノ彡┻━┻",
-            },
-            {
-                name: 'Gabriel Trujillo',
-                title: 'Tech Manager',
-                picture: '../../../../assets/img/man.png',
-                bio:
-                    "Here is where i'd put the bio ... if i had one! (ノಠ益ಠ)ノ彡┻━┻",
-            },
-            {
-                name: 'Gabriel Trujillo',
-                title: 'Tech Manager',
-                picture: '../../../../assets/img/man.png',
-                bio:
-                    "Here is where i'd put the bio ... if i had one! (ノಠ益ಠ)ノ彡┻━┻",
-            },
-            {
-                name: 'Gabriel Trujillo',
-                title: 'Tech Manager',
-                picture: '../../../../assets/img/man.png',
-                bio:
-                    "Here is where i'd put the bio ... if i had one! (ノಠ益ಠ)ノ彡┻━┻",
-            },
-            {
-                name: 'Gabriel Trujillo',
-                title: 'Tech Manager',
-                picture: '../../../../assets/img/man.png',
-                bio:
-                    "Here is where i'd put the bio ... if i had one! (ノಠ益ಠ)ノ彡┻━┻",
-            },
-            {
-                name: 'Gabriel Trujillo',
+                name: 'David Montoya',
                 title: 'Tech Manager',
                 picture: '../../../../assets/img/man.png',
                 bio:
