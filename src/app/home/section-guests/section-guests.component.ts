8f6f16f87d4c04f0beaecf6e7997423e8df829e6
Jorge Morales <jorge.iv.morales@gmail.com>
2019-08-15 12:29:08 -0500

    Corregido texto NodeSource

--- src/app/home/section-guests/section-guests.component.ts
+++ src/app/home/section-guests/section-guests.component.ts
@@ -1,74 +1,72 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-section-guests',
-  templateUrl: './section-guests.component.html',
-  styleUrls: ['./section-guests.component.scss']
+    selector: 'app-section-guests',
+    templateUrl: './section-guests.component.html',
+    styleUrls: ['./section-guests.component.scss'],
 })
 export class SectionGuestsComponent implements OnInit {
+    imageIndex: number;
+    guests: any;
+    pageX: number;
 
-  imageIndex: number;
-  guests: any;
-  pageX: number;
+    constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-    this.guests = [
-      {
-        image: '/assets/img/special-guests/srdjan-josipovic.png',
-        talk: 'Keynote: Why enterprises embrace Node.js',
-        name: 'Serge Josipovic',
-        company: 'Software Achitecture - Rockwell Automation',
-      },
-      {
-        image: '/assets/img/special-guests/adrian-estrada.png',
-        talk: 'Detangling multithreading in Node.js',
-        name: 'Adrian Estrada',
-        company: 'Node Source',
-      },
-      {
-        image: '/assets/img/special-guests/pete-kinstry.png',
-        talk: 'Node projects at scale',
-        name: 'Peter Mc Kinstry',
-        company: 'Technical Director - Globant',
-      },
-      {
-        image: '/assets/img/special-guests/rob-howard.png',
-        talk: 'Node projects at scale',
-        name: 'Rob Howard',
-        company: 'Technical Director - Globant',
-      },
-      {
-        image: '/assets/img/special-guests/kevin-fonner.png',
-        talk: 'Controlling the world with Node',
-        name: 'Kevin Fonner',
-        company: 'Rockwell Automation',
-      },
-    ];
-    this.imageIndex = 0;
-  }
-
-  public modifyImageIndex(digit: number): void  {
-    let index = this.imageIndex + digit;
-    if (index < 0) {
-      index = this.guests.length - 1;
+    ngOnInit() {
+        this.guests = [
+            {
+                image: '/assets/img/special-guests/srdjan-josipovic.png',
+                talk: 'Keynote: Why enterprises embrace Node.js',
+                name: 'Serge Josipovic',
+                company: 'Software Achitecture - Rockwell Automation',
+            },
+            {
+                image: '/assets/img/special-guests/adrian-estrada.png',
+                talk: 'Detangling multithreading in Node.js',
+                name: 'Adrian Estrada',
+                company: 'NodeSource',
+            },
+            {
+                image: '/assets/img/special-guests/pete-kinstry.png',
+                talk: 'Node projects at scale',
+                name: 'Peter Mc Kinstry',
+                company: 'Technical Director - Globant',
+            },
+            {
+                image: '/assets/img/special-guests/rob-howard.png',
+                talk: 'Node projects at scale',
+                name: 'Rob Howard',
+                company: 'Technical Director - Globant',
+            },
+            {
+                image: '/assets/img/special-guests/kevin-fonner.png',
+                talk: 'Controlling the world with Node',
+                name: 'Kevin Fonner',
+                company: 'Rockwell Automation',
+            },
+        ];
+        this.imageIndex = 0;
     }
-    if (index >= this.guests.length) {
-      index = 0;
+
+    public modifyImageIndex(digit: number): void {
+        let index = this.imageIndex + digit;
+        if (index < 0) {
+            index = this.guests.length - 1;
+        }
+        if (index >= this.guests.length) {
+            index = 0;
+        }
+        this.imageIndex = index;
     }
-    this.imageIndex = index;
-  }
 
-  public touchStart(e: TouchEvent) {
-    const { pageX } = e.touches[0];
-    this.pageX = pageX;
-  }
+    public touchStart(e: TouchEvent) {
+        const { pageX } = e.touches[0];
+        this.pageX = pageX;
+    }
 
-  public touchEnd(e: TouchEvent) {
-    const { pageX } = e.changedTouches[0];
-    const left = (this.pageX - pageX) > 0;
-    this.modifyImageIndex(left ? 1 : -1);
-  }
-
+    public touchEnd(e: TouchEvent) {
+        const { pageX } = e.changedTouches[0];
+        const left = this.pageX - pageX > 0;
+        this.modifyImageIndex(left ? 1 : -1);
+    }
 }
